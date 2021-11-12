f0583b6783957c2ed5dc8a34556759f8aca3665f
NaiaraSantoss <naiara.santos@live.com>
2021-11-12 17:35:18 -0300

    v1.0

---
+++ src/test/java/com/login/api/loginapi/LoginapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.api.loginapi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
